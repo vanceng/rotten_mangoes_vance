04ce571456bd30cbf83a3a534a4757bac3284613
Vance Ng <vancejng@gmail.com>
2015-04-28 23:27:00 +0000

    first commit

---
+++ rotten_mangoes_vance/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoesVance::Application.config.secret_key_base = '0d1c1a26b391a39b4e4a8b612e538f3f6a25a9f1f90aae10dd3219a56e2fe3fa20c03460f612711c80e03774ecd02a3ebac5b4ef843764b84e11b1defc08734c'
