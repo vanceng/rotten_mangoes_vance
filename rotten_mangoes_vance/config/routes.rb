efecd519c449492a3b88f79e6fbafa342d480ef4
Vance Ng <vancejng@gmail.com>
2015-04-29 01:53:58 +0000

    Added Reviews

--- rotten_mangoes_vance/config/routes.rb
+++ rotten_mangoes_vance/config/routes.rb
@@ -1,10 +1,21 @@
 RottenMangoesVance::Application.routes.draw do
 
+  get "reviews/new"
+  get "reviews/create"
+  get "sessions/new"
+  get "sessions/create"
+  get "users/new"
+  get "users/create"
   get "movies/index"
   get "movies/show"
   get "movies/new"
   get "movies/edit"
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end 
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
 end
